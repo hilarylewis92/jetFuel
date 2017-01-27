01af3eed69fa490cf04bf0756345b51cf3de1bfc
Ian Lancaster <ianclancaster@gmail.com>
2017-01-27 11:25:13 -0700

    require path in server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const md5 = require('md5')
+const path = require("path")
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
