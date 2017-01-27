682db7218e9e9f93cd70eb8ccce09e97a086a5d7
Hilary Lewis <hilarylewis92@gmail.com>
2017-01-26 21:44:57 -0700

    Add styles

--- src/App.js
+++ src/App.js
@@ -6,13 +6,34 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1
+          className="title">
+          Jet Fuel
+        </h1>
+          <img
+            className="rocket-ship"
+            src="./icons/transport-2.svg"
+            alt="rocket ship"
+          />
+          <form>
+            <label
+              for="folder-label">
+              Add a new folder to house similar URLs:
+            </label>
+            <input
+              id="folder-input"
+              placeholder="Full URL folder...">
+            </input>
+            <button
+              className="folder-submit-button"
+              type="submit">
+              Submit
+            </button>
+          </form>
+
+          <ul
+            id="folders-container">
+          </ul>
       </div>
     );
   }
