60f508b840e17a93c50296e7bfde04b7d2de65ba
Hilary Lewis <hilarylewis92@gmail.com>
2017-01-26 20:54:12 -0700

    Create folders get route

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const md5 = require('md5');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', process.env.PORT || 3000)
+app.locals.title = 'Jet Fuel'
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.get('/api/folders', (req, res) => {
+  database('folders').select()
+    .then((folders) => {
+      res.status(200).json(folders)
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is runing on ${app.get('port')}.`)
+})
